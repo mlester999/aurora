29976c73b95ef5b0be0c588cb23494e5d5f0564a
mlester999 <mister6virgo@gmail.com>
2022-08-04 17:10:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".auto-type", {
+  strings: [
+    "Are you a nature lover?",
+    "Are you a thrill seeker?",
+    "Are you an adventure person?",
+    "Have fun with us now!",
+  ],
+  typeSpeed: 80,
+  backSpeed: 80,
+  loop: true,
+});
